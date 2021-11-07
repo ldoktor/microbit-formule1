fc0ea51d8580a04e15f9a56f4501e843890b7fd0
Lukáš Doktor <ldoktor@redhat.com>
2021-11-07 15:31:11 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,45 +1,40 @@
-def on_button_pressed_b():
-    global gear, ratio, ratios, rev
-    if gear >= 6:
+def shift(new_gear):
+    # Avoid invalid gears
+    if new_gear > 6 or new_gear < 0:
         return
-    gear = gear + 1
-    old = ratio
-    ratio = ratios[gear]
-    rev = old * rev / ratio
-    basic.show_number(gear)
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-def on_button_pressed_ab():
+    # Change the ratio, gear and adjust revs
     global gear, ratio, ratios, rev
-    if gear <= 0:
-        return
-    gear = gear - 1
     old = ratio
-    ratio = ratios[gear]
+    ratio = ratios[new_gear]
     rev = old * rev / ratio
-    basic.show_number(gear)
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
+    gear = new_gear
 
+def shift_up():
+    global gear
+    shift(gear + 1)
+input.on_button_pressed(Button.B, shift_up)
+
+def shift_down():
+    global gear
+    shift(gear - 1)
+input.on_button_pressed(Button.AB, shift_down)
+
+# Initialize environment
 exit = False
 distance = 0
 speed = 0
-ratio = 0
-ratios: List[number] = []
 gear = 0
 # rev 600 - 14000
 rev = 600
 gear = 1
-ratios = [0, 0.00357, 0.00714, 0.0107, 0.0143, 0.0179, 0.0257]
+ratio = 0
+ratios: List[number] = [0, 0.00357, 0.00714, 0.0107, 0.0143, 0.0179, 0.0257]
 music.set_tempo(900)
 #music.change_tempo_by(1000)
 
-def on_forever():
+def physics():
     global rev, speed, distance, gear, ratio, exit
-    #serial.write_value("rev", rev)
-    #serial.write_value("speed", speed)
-    #serial.write_value("distance", distance)
-    #serial.write_value("gear", gear)
-    # led.plot_bar_graph(rev, 14000)
+    # Exit conditions
     if rev < 600:
         rev = 600
     elif rev > 14000:
@@ -49,13 +44,14 @@ def on_forever():
         basic.show_number(distance)
         basic.pause(1000)
         control.reset()
-    elif distance >= 25:
+    if distance >= 25:
         exit = True
         basic.show_string("Finish!")
         basic.show_number(control.millis() / 1000)
         basic.pause(1000)
         control.reset()
     if input.button_is_pressed(Button.A):
+        # Acceleration
         if rev < 2500:
             rev = rev * 1.015
         if rev < 5000:
@@ -67,21 +63,24 @@ def on_forever():
         else:
             rev = rev * 1.020
     else:
+        # Idling
         rev = rev * 0.99
     speed = rev * ratio
-    # wind
+    # Wind
     rev = rev * Math.map(speed, 0, 400, 1, 0.981)
     distance = distance + 0.0001 * speed
     basic.pause(10)
-basic.forever(on_forever)
+basic.forever(physics)
 
-def on_forever2():
+def engine_sound():
     music.play_tone(rev / 10, music.beat(BeatFraction.WHOLE))
     music.play_tone(rev / 10 + 50, music.beat(BeatFraction.WHOLE))
-    music.play_tone(rev / 10 + 150, music.beat(BeatFraction.WHOLE))
-basic.forever(on_forever2)
+    #music.play_tone(rev / 10 + 150, music.beat(BeatFraction.WHOLE))
+basic.forever(engine_sound)
 
-def on_forever3():
+def draw_hud():
+    # Keep drawing HUD until exit
+    # RPMs, speed, gear, circuit position, all laps
     global exit
     if exit:
         return
@@ -96,4 +95,4 @@ def on_forever3():
         led.plot(3, i)
     for i in range((distance / 5)):
         led.plot(4, i)
-basic.forever(on_forever3)
+basic.forever(draw_hud)
--- main.ts
+++ main.ts
@@ -1,46 +1,40 @@
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
-    if (gear >= 6) {
+function shift(new_gear: number) {
+    //  Avoid invalid gears
+    if (new_gear > 6 || new_gear < 0) {
         return
     }
     
-    gear = gear + 1
-    let old = ratio
-    ratio = ratios[gear]
-    rev = old * rev / ratio
-    basic.showNumber(gear)
-})
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    //  Change the ratio, gear and adjust revs
     
-    if (gear <= 0) {
-        return
-    }
-    
-    gear = gear - 1
     let old = ratio
-    ratio = ratios[gear]
+    ratio = ratios[new_gear]
     rev = old * rev / ratio
-    basic.showNumber(gear)
+    gear = new_gear
+}
+
+input.onButtonPressed(Button.B, function shift_up() {
+    
+    shift(gear + 1)
 })
+input.onButtonPressed(Button.AB, function shift_down() {
+    
+    shift(gear - 1)
+})
+//  Initialize environment
 let exit = false
 let distance = 0
 let speed = 0
-let ratio = 0
-let ratios : number[] = []
 let gear = 0
 //  rev 600 - 14000
 let rev = 600
 gear = 1
-ratios = [0, 0.00357, 0.00714, 0.0107, 0.0143, 0.0179, 0.0257]
+let ratio = 0
+let ratios = [0, 0.00357, 0.00714, 0.0107, 0.0143, 0.0179, 0.0257]
 music.setTempo(900)
 // music.change_tempo_by(1000)
-basic.forever(function on_forever() {
+basic.forever(function physics() {
     
-    // serial.write_value("rev", rev)
-    // serial.write_value("speed", speed)
-    // serial.write_value("distance", distance)
-    // serial.write_value("gear", gear)
-    //  led.plot_bar_graph(rev, 14000)
+    //  Exit conditions
     if (rev < 600) {
         rev = 600
     } else if (rev > 14000) {
@@ -50,7 +44,9 @@ basic.forever(function on_forever() {
         basic.showNumber(distance)
         basic.pause(1000)
         control.reset()
-    } else if (distance >= 25) {
+    }
+    
+    if (distance >= 25) {
         exit = true
         basic.showString("Finish!")
         basic.showNumber(control.millis() / 1000)
@@ -59,6 +55,7 @@ basic.forever(function on_forever() {
     }
     
     if (input.buttonIsPressed(Button.A)) {
+        //  Acceleration
         if (rev < 2500) {
             rev = rev * 1.015
         }
@@ -74,22 +71,25 @@ basic.forever(function on_forever() {
         }
         
     } else {
+        //  Idling
         rev = rev * 0.99
     }
     
     speed = rev * ratio
-    //  wind
+    //  Wind
     rev = rev * Math.map(speed, 0, 400, 1, 0.981)
     distance = distance + 0.0001 * speed
     basic.pause(10)
 })
-basic.forever(function on_forever2() {
+// music.play_tone(rev / 10 + 150, music.beat(BeatFraction.WHOLE))
+basic.forever(function engine_sound() {
     music.playTone(rev / 10, music.beat(BeatFraction.Whole))
     music.playTone(rev / 10 + 50, music.beat(BeatFraction.Whole))
-    music.playTone(rev / 10 + 150, music.beat(BeatFraction.Whole))
 })
-basic.forever(function on_forever3() {
+basic.forever(function draw_hud() {
     let i: number;
+    //  Keep drawing HUD until exit
+    //  RPMs, speed, gear, circuit position, all laps
     
     if (exit) {
         return
